e734fb26848318101234cfc27dffa2b95054b843
Hari2020codes <160399623+Hari2020codes@users.noreply.github.com>
2025-10-27 16:19:08 +0530

    Add files via upload

---
+++ php/registration.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        enter id<input type="text" name="eid"><br>
+        enter name<input type="text" name="ename"><br>
+        enter salary<input type="text" name="salary"><br>
+       Submit <input type="submit" name="submit">
+      </form>
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="web";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn)
+{
+    echo"not connected <br>";
+}
+else{
+    echo "connected successfully<br>";
+}
+
+$id=$_POST['eid'];
+$salary=$_POST['salary'];
+$name=$_POST['ename'];
+
+$query="INSERT INTO test VALUES('$id','$name','$salary')";
+$res=mysqli_query($conn, $query);
+if($res)
+{
+    echo" submitted";
+}
+else{
+    echo "not submitted";
+}
+?>
+
+
+</body>
+</html>

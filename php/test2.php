2b0eb792fe557dede3b839c1cf5aea171d7043ca
Hari2020codes <160399623+Hari2020codes@users.noreply.github.com>
2025-11-17 16:18:40 +0530

    Add files via upload

---
+++ php/test2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table{
+            background-color:gold;
+            height:200;
+            width:200;
+        }
+        body{
+            background-color:silver;
+        }
+        </style>
+</head>
+<body style="color:darkred;"><form method="POST" action="">
+    <center><table border="2">
+       <tr> <h1 style="color:blue;"><u>REGISTRATION FORM</u></h1></tr>
+       <tr>
+        <td> enter your name :<br></td>
+        <td><input type="text" name="name" id="na"><br></td>
+        </tr>
+       </tr>
+       <tr><td> enter your password :<br></td>
+            <td> <input type="password" name="password" id="pass"><br></td>
+       </tr>
+       <tr><td> <input type="submit" value="SUBMIT"> </td>
+          <td><center><input type="reset" value="RESET"></center></td>
+        </tr>       
+
+</center></table>
+</form>
+</body>
+</html>
